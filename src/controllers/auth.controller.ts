679c5615123908312f5f1b41be3edccbfc8c659a
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-05-26 13:48:19 +0100

    fix: correct import path for User model in auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from 'models/User.js';
+import User from '../models/User.js';
 import crypto from 'node:crypto';
 
 import { generateToken } from '../utils/generateToken.js';
