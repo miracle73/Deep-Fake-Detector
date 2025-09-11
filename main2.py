011967662eb220c6ce90573167619f98e1c74cfd
Mirack <50253665+miracle73@users.noreply.github.com>
2025-09-11 00:20:19 +0000

    Adjustment made

--- main2.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-# Load the model
-detector = pipeline("image-classification", model="Organika/sdxl-detector")
-
-# Use it
-result = detector("./firstImage.png")
-print(result[0])
